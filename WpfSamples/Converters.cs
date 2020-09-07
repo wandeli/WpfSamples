e1b26aca2057431bb906e723b1d234baddf35a49
wandeli <wandeli@digisky.com>
2020-09-07 11:11:52 +0800

    添加项目文件。

---
+++ WpfSamples/Converters.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WpfSamples
+{
+    public class WidthConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is double width)
+            {
+                return -width;
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+
+    public class DurationConverter : IValueConverter
+    {
+        public float WidthForSecond = 50;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double width)
+            {
+                int time = (int)(width / WidthForSecond);
+                Duration duration = new Duration(new TimeSpan(0,0, time));
+                return duration;
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
