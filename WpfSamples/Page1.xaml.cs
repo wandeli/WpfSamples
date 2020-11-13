507f39be47d975e15c3674cf71cb215fa9571880
wandeli <wandeli@digisky.com>
2020-11-13 11:39:34 +0800

    -up

--- WpfSamples/Page1.xaml.cs
+++ WpfSamples/Page1.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +19,49 @@ namespace WpfSamples
     /// <summary>
     /// Page1.xaml 的交互逻辑
     /// </summary>
+    /// 
+    public class MenuItemData
+    {
+        public MenuItemData()
+        {
+            Items = new ObservableCollection<MenuItemData>();
+        }
+        public string Name { get; set; }
+        public int Type { get; set; }
+        public ObservableCollection<MenuItemData> Items { get; set; }
+    }
+
     public partial class Page1 : Page
     {
+
+        public ObservableCollection<MenuItemData> MenuItems { get; set; }
+
         public Page1()
         {
             InitializeComponent();
+            MenuItems = new ObservableCollection<MenuItemData>();
+            MenuItemData child1 = new MenuItemData()
+            {
+                Name = "child1"
+            };
+            MenuItemData child11 = new MenuItemData()
+            {
+                Name = "child11"
+            };
+            MenuItemData child12 = new MenuItemData()
+            {
+                Name = "child12"
+            };
+            child1.Items.Add(child11);
+            child1.Items.Add(child12);
+            MenuItemData child2 = new MenuItemData()
+            {
+                Name = "child2"
+            };
+            
+            MenuItems.Add(child1);
+            MenuItems.Add(child2);
+            DataContext = this;
         }
     }
 }
